01fb74f3c38b9d39866c4abd83607581623dacb2
Ashish Santikari <mailto.ashishsan@gmail.com>
2020-05-01 13:53:14 +0200

    Refactor QuestionDetail component

--- src/components/QuestionsDetail/index.js
+++ src/components/QuestionsDetail/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Wrapper = styled.div`
   padding: 10px;
@@ -17,18 +17,34 @@ const Choices = styled.form`
 const ChoiceRow = styled.div`
   display: flex;
   border-top: 1px solid black;
-  padding: 10px;
+  padding: 1rem;
   &:last-child {
     border-bottom: 1px solid black;
   }
+  ${(props) =>
+    props.isHeader &&
+    css`
+      font-weight: bold;
+    `}
+`;
+
+const ActionsRow = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  padding: 1rem;
 `;
 
 const Option = styled.div`
-  width: 50%;
+  width: 75%;
 `;
 
 const Vote = styled.div`
-  width: 50%;
+  width: 25%;
+`;
+
+const Button = styled.button`
+  outline: 1px solid green;
+  padding: 0.5rem;
 `;
 
 const QuestionDetail = ({ question, choices }) => {
@@ -36,14 +52,21 @@ const QuestionDetail = ({ question, choices }) => {
     <Wrapper>
       <QuestionHeader>Question: {question} </QuestionHeader>
       <Choices>
+        <ChoiceRow isHeader>
+          <Option>Choice</Option>
+          <Vote># of Votes</Vote>
+        </ChoiceRow>
         {choices &&
           choices.map((choice) => (
             <ChoiceRow key={choice.choice}>
               <Option>{choice.choice}</Option>
-              <Vote>{choice.votes}</Vote>
+              <Vote>{choice.votes} Votes</Vote>
             </ChoiceRow>
           ))}
       </Choices>
+      <ActionsRow>
+        <Button>Save Vote</Button>
+      </ActionsRow>
     </Wrapper>
   );
 };
