90d283fccc3e1cadf62841c4fb286e3c6f74c255
Ashish Santikari <mailto.ashishsan@gmail.com>
2020-05-01 13:47:19 +0200

    Wrapper for App Container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
+const AppWrapper = styled.div`
+  width: 50vw;
+  margin: auto;
+  @media (max-width: 768px) {
+    width: 90vw;
+    margin: auto;
+  }
+`;
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path={`/detail/:questionId`}>
-          <Detail />
-        </Route>
-        <Route path="*">
-          <h1>404: NOT FOUND</h1>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path={`/detail/:questionId`}>
+            <Detail />
+          </Route>
+          <Route path="*">
+            <h1>404: NOT FOUND</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
