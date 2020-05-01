083ebc8c8d20308170ba1fe9353e06256faea0b1
Ashish Santikari <mailto.ashishsan@gmail.com>
2020-05-01 11:51:20 +0200

    Setup Home, Detail and Not found routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home Page</h1>
+        </Route>
+        <Route path={`/detail/:questionId`}>
+          <h1>Detail page</h1>
+        </Route>
+        <Route path="*">
+          <h1>404: NOT FOUND</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
